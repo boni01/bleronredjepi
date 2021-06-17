ec20224c6b2dd41ae2ae3f6ff9e4d9a3dd250672
boni01 <bleron.rexhepi.ch@gmail.com>
2021-06-17 14:25:21 +0200

    better loading screen with template animation

--- public/js/loader.js
+++ public/js/loader.js
@@ -6,8 +6,9 @@ $(window).on("load", function(){
 function removeLoading() {
 
     $("#loading").remove()
-
+    $('body').addClass('is-preload')
     $("#wrapper").fadeIn(500, function(){
         $("#wrapper").show();
     });
+    $('body').removeClass("is-preload")
 }
